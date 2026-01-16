3032db3b016e6f42748f1407c872337516341453
P.G Pathum Himesha Prasad Subasigha <prasadhphs@gmail.com>
2026-01-16 15:29:29 +0530

    updated the code

--- src/main/java/com/shopping/dao/DBConnection.java
+++ src/main/java/com/shopping/dao/DBConnection.java
@@ -5,24 +5,21 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-    // 1. Database URL (Make sure your database name is 'online_store')
+    
     private static final String URL = "jdbc:mysql://localhost:3306/online_store";
     
-    // 2. Database Username (Default is usually 'root')
+    
     private static final String USER = "root"; 
     
-    // -------------------------------------------------------------------------
-    // ⚠️ TEAM MEMBERS: CHANGE THE PASSWORD BELOW TO YOUR OWN MYSQL PASSWORD ⚠️
-    // If your password is "1234", change "hash1" to "1234"
-    // -------------------------------------------------------------------------
+
     private static final String PASSWORD = "12345"; 
 
     public static Connection getConnection() {
         Connection connection = null;
         try {
-            // Load the MySQL Driver
+            
             Class.forName("com.mysql.cj.jdbc.Driver");
-            // Establish the connection
+            
             connection = DriverManager.getConnection(URL, USER, PASSWORD);
             System.out.println("Database Connected Successfully!");
         } catch (ClassNotFoundException e) {
